5e974558cdc693d2b1921f8d69699ee34e51fd13
Yoko Ishikawa <ishikawa.yoko.io5@is.naist.jp>
2016-10-14 19:48:37 +0900

    Add files via upload

--- comic/forms.py
+++ comic/forms.py
@@ -12,31 +12,32 @@ BUBBLE_NUM = (
     ('0','0こ'),
     ('1','1こ'),
     ('2','2こ'),
-    ('3','3こ'),
-    ('4','4こ以上'),
+    ('3','3こ以上'),
 )
 
 USE = (
-    ('0','1人'),
-    ('1','2人以上')
+    ('0','0人'),
+    ('1','1人'),
+    ('2','2人以上')
 )
 
 PONE = (
     ('0','ポジティブ'),
-    ('1','ネガティブ')
+    ('1','ネガティブ'),
+    ('2','ニュートラル')
 )
 
 class UseForm(forms.Form):
     flag = forms.ChoiceField(
         label = "喋っている人",
-        required=False,
+        required=True,
         widget=forms.RadioSelect(),
         choices=USE,
     )
 
 
 class PositiveForm(forms.Form):
-    flag = forms.ChoiceField(
+    pn = forms.ChoiceField(
         label = "ポジネガ判定",
         required=False,
         widget=forms.RadioSelect(),
@@ -55,7 +56,7 @@ class UtterForm(forms.Form):
 class SpeakerForm(forms.Form):
     bubble = forms.ChoiceField(
         label = "吹き出し数",
-        required=False,
+        required=True,
         widget=forms.RadioSelect(),
         choices=BUBBLE_NUM,
     )
--- comic/views.py
+++ comic/views.py
@@ -3,18 +3,64 @@ from django.http.response import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.http import QueryDict
 from django.forms.formsets import formset_factory
-from . import forms#,models
+from . import forms,models
 import re,glob,random
 
+#トップページを設定
 def index(request):
+    person_num=0
+    bubble_num=0
+    flags=0
+    message=""
+    image="img/test.png"
+    ### フォームを読み込む
+    # このコマを使うか使わないか（喋っている人が１人以上か否か）
     use_form=forms.UseForm(request.GET or None)
-    utter_form=forms.UtterForm(request.GET or None)
+    # 吹き出しが何個か
     speaker_form=forms.SpeakerForm(request.GET or None)
-    posi_form=forms.PositiveForm(request.GET or None)
+    print(request.POST.keys())
+    if "confirm" in request.POST:
+        if "flag" in request.POST.keys() and "bubble" in request.POST.keys():
+            print(request.POST['flag'])
+            print(request.POST['bubble'])
+            message = "確認：喋っている人："+request.POST['flag']+"人, 吹き出し数："+request.POST['bubble']+"こ"
+            person_num=request.POST['flag']
+            bubble_num=request.POST['bubble']
+            request.session['pre_pnum']=person_num
+            request.session['pre_bnum']=bubble_num
+        else:
+            message = '全ての項目を入力してください'
+    elif "regist" in request.POST:
+        print(request.session.keys())
+        person_num=request.session['pre_pnum']
+        bubble_num=request.session['pre_bnum']
+        print(person_num)
+        print(bubble_num)
+        if int(person_num) >=1 and int(bubble_num) >=0:
+            print("使える")
+            flags=1
+        else:
+            print("使えない")
+            flags=0
+        models.ComicInfo.objects.create(
+            file_name=image,
+            person_num=person_num,
+            bubble_num=bubble_num,
+            availability=flags,
+        )
+        print("登録")
+        message="登録しました"
+        person_num=""
+        bubble_num=""
+    
+    # 変数をページに引き渡す
     d={
         'use_form' : use_form,
-        'utter_form' : utter_form,
         'speaker_form' : speaker_form,
-        'posi_form' : posi_form,
+        'image' : image,
+        'message' : message,
+        'person_num' : person_num,
+        'bubble_num' : bubble_num,
     }
+    # index.html で表示させる
     return render(request, 'index.html', d)
